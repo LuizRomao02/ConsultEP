4b946957506f994932a42bcba2134c508b6951a5
LuizRomao02 <luizromao7@outlook.com>
2024-12-11 12:09:12 -0300

    first commit

---
+++ src/main/java/com/luizromao/consultep/ConsultEpApplication.java
@@ -0,0 +1,13 @@
+package com.luizromao.consultep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultEpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultEpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizromao/consultep/ConsultEpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizromao.consultep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultEpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
